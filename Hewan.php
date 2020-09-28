e548891f84b5f84eb3711f1b59c84d0890936aa5
Jundullah H. R <jundihanif77@gmail.com>
2020-09-28 23:08:24 +0700

    Add property and method atraksi()

--- Hewan.php
+++ Hewan.php
@@ -3,4 +3,10 @@
 trait Hewan
 {
 	public $nama, $darah = 50, $jumlahKaki, $keahlian;
+	
+	public function atraksi()
+	{
+		echo $this->nama . " sedang " . $this->keahlian;
+		return $this->nama . " sedang " . $this->keahlian;
+	}
 }
