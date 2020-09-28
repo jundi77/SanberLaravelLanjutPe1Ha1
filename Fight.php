bbcee20171bb19b1fb65aa8f1958e128714d65d4
Jundullah H. R <jundihanif77@gmail.com>
2020-09-28 23:19:47 +0700

    add property serang, diserang

--- Fight.php
+++ Fight.php
@@ -5,14 +5,28 @@ trait Fight
 	use Hewan;
 	public $attackPower, $defencePower;
 
-	public function serang($obj_ke)
+	public function serang($target)
 	{
-		// echo $this->na
+		try {
+			echo $this->nama . " sedang menyerang " . $target->nama;
+			$target->diserang($this);
+		}
+		catch(Exception $e) {
+			echo "Obyek yang digunakan harus obyek yang menggunakan trait Fight.\n";
+			return $e;
+		}
 		return $this;
 	}
 
-	private function diserang($obj_dari)
+	private function diserang($penyerang)
 	{
-
+		try {
+			echo $this->nama . " sedang diserang ";
+			$this->darah = $this->darah - ($penyerang / $this->defencePower);
+		}
+		catch(Exception $e) {
+			echo "Obyek yang digunakan harus obyek yang menggunakan trait Fight.\n";
+			return $e;
+		}
 	}
 }
